647a193790ee832da4e18cd0087a3764b178c743
Sinchana-Rai <sinchana.srai@gmail.com>
2024-12-04 20:31:55 -0500

    small code change

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,6 @@ function Header() {
   return (<h1>Simple React Application</h1>);
 }
 
- function Article() {
-  return(
-      <>
-      <h2>On the Street in Brooklyn</h2>
-      <p style= {{ textAlign: right }} >
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      </>
-  );
-}
-
 function Content(props) {
   return (<p style={{color: props.color}}>{props.text}</p>);
 }
@@ -31,8 +20,7 @@ function App() {
     <>
       
       <Header />
-      <Article />
-      <Content color="blue" text="This is my first React Application!" />
+       <Content color="blue" text="This is my first React Application!" />
       <Content color="red" text="Wish me luck..." />
       <Content color="green" text="I think I've got it!" />
       <Footer />
